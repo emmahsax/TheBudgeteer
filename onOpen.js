8d6f8aee7a6a9e788b1b15d8dc4a6c333c9c2826
Emma Sax <emma.sax4+git@gmail.com>
2021-04-08 12:49:55 -0500

    Be able to clear and copy budgets

--- onOpen.js
+++ onOpen.js
@@ -2,11 +2,14 @@ function onOpen() {
   refreshVisibleBudgets(false);
 
   var entries = [
-    {name: "Refresh Visible Budgets", functionName: "refreshVisibleBudgets"},
-    {name: "Show All Budgets", functionName: "showAllBudgets"},
-    {name: "Create New Budget", functionName: "createBudget"},
+    {name: "Refresh Visible Budget Categories", functionName: "refreshVisibleBudgets"},
+    {name: "Show All Budget Categories", functionName: "showAllBudgets"},
     {name: "Update Monthly Budget", functionName: "updateBudget"},
-    {name: "Delete Budget", functionName: "deleteBudget"},
+    {name: "Clear Current Month's Budgets", functionName: "clearCurrentBudgets"},
+    {name: "Clear Future Months' Budgets", functionName: "clearFutureBudgets"},
+    {name: "Copy Last Month's Budgets to Current Month", functionName: "copyLastMonthsBudgets"},
+    {name: "Create Budget Category", functionName: "createBudget"},
+    {name: "Delete Budget Category", functionName: "deleteBudget"},
     {name: "Sort Monthly Transactions by Date", functionName: "sortTransactionsByDate"},
     {name: "Sort Monthly Transactions by Category", functionName: "sortTransactionsByCategory"}
   ];
